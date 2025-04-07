9bdff5c7dbdc0b7b84b1363d0655ee0b9b39db9f
sakshi052 <sakshiranjan421@gmail.com>
2025-04-07 23:56:03 +0530

    comment

--- src/pages/page3/Pg3.jsx
+++ src/pages/page3/Pg3.jsx
@@ -1,55 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import LovingHands from "../../assets/pg3/LovingHands.png";
-import "./pg3.css"
+import "./pg3.css";
 import Pattern1 from "../../assets/pg2/Pattern.png";
 
-function Pg3(){
-    return(
-        <div>
-            <div className="pg3-input">
-                
-            <form>
-                <div className="ques1">
-                <label className="pg3-ip" for="userName">What shall we call you?</label><br></br>
-                </div>
-                <input className="pg3-blank" type="text" id="userName" name="userName" required></input><br></br>
-                
-            </form>
-            <form>
-                <div className="ques2">
-                <label className="pg3-ip" for="Friend">Who's sharing the frame with you?</label><br></br>
-                <input className="pg3-blank" type="text" id="friend" name="Friend" required></input><br></br>
-                </div>   
-            </form>
-            <form>
-            <div className="ques3">
-            <label className="pg3-ip" for="note">Note ❤️</label><br></br>
-            <input className="pg3-blank" type="text" id="note" name="note" maxlength="100" required></input>
-            </div>
-            <button onClick={() => (window.location.href = "/pg4")} className="submit-btn" type="submit">Submit</button>
-            </form>
-            </div>
+function Pg3() {
+  const [formData, setFormData] = useState({
+    userName: "",
+    partner: "",
+    note: ""
+  });
 
-            <div className="pg3-upload">
-                <div className="img2">
-                <img src={LovingHands} className="pg3_hands" alt="LovingHands"/>
-                </div>
-                <form>
-                <input  type="file" id="fileUpload" name="fileUpload" required></input>
-                <button className="pg3-btn">Upload</button>
-                </form>
-                
-            </div>
-            
-            <div>
-                <button onClick={() => (window.location.href = "/pg2")} className="bck-btn">back</button>
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
-            <div className="bottom" >
-                        <img className="pattern" src= {Pattern1}/>
-                       </div>
+  return (
+    <div>
+      <div className="pg3-input">
+        <form>
+          <label className="pg3-ip" htmlFor="userName">What shall we call you?</label><br />
+          <input
+            className="pg3-blank"
+            type="text"
+            name="userName"
+            userName={formData.userName}
+            onChange={handleChange}
+            required
+          /><br />
+
+          <label className="pg3-ip" htmlFor="Friend">Who's sharing the frame with you?</label><br />
+          <input
+            className="pg3-blank"
+            type="text"
+            name="partner"
+            partner={formData.partner}
+            onChange={handleChange}
+            required
+          /><br />
+
+          <label className="pg3-ip" htmlFor="note">Note ❤️</label><br />
+          <input
+            className="pg3-blank"
+            type="text"
+            name="note"
+            maxLength="200"
+            note={formData.note}
+            onChange={handleChange}
+            required
+          />
+            <br></br>
+          <button
+            onClick={() => (window.location.href = "/pg4")}
+            className="submit-btn"
+            type="submit"
+          >
+            🌹 I'm In!
+          </button>
+        </form>
+      </div>
+
+      <div className="pg3-upload">
+        <div className="img2">
+          <img src={LovingHands} className="pg3_hands" alt="LovingHands" />
         </div>
-    )
+        <form>
+          <input type="file" id="fileUpload" name="fileUpload" required />
+          <button className="pg3-btn">Upload</button>
+        </form>
+      </div>
+
+      <div>
+        <button 
+          onClick={() => (window.location.href = "/pg2")}
+          className="bck-btn"
+        >
+          Go Back
+        </button>
+      </div>
+
+      <div className="bottom">
+        <img className="pattern" src={Pattern1} />
+      </div>
+    </div>
+  );
 }
 
-export default Pg3
+export default Pg3;
--- src/pages/page8/Pg8.jsx
+++ src/pages/page8/Pg8.jsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect } from "react";
 import Pattern1 from "../../assets/pg2/Pattern.png";
 import "./pg8.css"
+import { useLocation } from "react-router-dom";
+
 
 function Pg8(){
+
+    const location = useLocation();
+  const { userName, partner, note } = location.state || {};
     return(
         <div>
             <div className="printing">
                 <h3 className="msg">Your Photo is getting printed!</h3>
+                <p><strong>Name:</strong> {userName}</p>
+      <p><strong>Email:</strong> {partner}</p>
+      <p><strong>Age:</strong> {note}</p>
             </div>
 
-            <div className="photostrip"></div>
+            
+                
+            
 
             <div className="stripBox">
             <div className="outerBox">
                 <div className="innerBox">
-
+                    <div className="strip">
+                    <div className=" photo pic1"></div>
+                    <div className="photo pic2"></div>
+                    <div className="photo pic3"></div>
+                    </div>
+                    
                 </div>
+                
             </div>
+            </div>
+
+            <div className="pg8-btn">
+                <button className="collect-btn" onClick={() => (window.location.href = "/pg9")}>Collect</button>
             </div>
             
             <div className="bottom" >
